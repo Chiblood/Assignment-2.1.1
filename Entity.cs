40af632c05c496990c49f9ac537bc29309300eea
Chiblood <chi.blood6@gmail.com>
2025-09-02 11:35:32 -0400

    Fix formatting and structure of the Entity class definition

--- Entity.cs
+++ Entity.cs
@@ -7,21 +7,23 @@ Design a class hierarchy of your choice. (Need only class files )
         Mob
             Animal
 */
-
-// Base class, restricted from instantiation
-public abstract class Entity(int id, string description)
+namespace Assignment2._1._1
 {
-    public int Id { get; set; } = id;
-    public string Description { get; set; } = description;
-
-    // public Entity(int id, string description)
-    // {
-    //     Id = id;
-    //     Description = description;
-    // }
-
-    public virtual void DisplayInfo()
+    // Base class, restricted from instantiation
+    public abstract class Entity(int id, string description)
     {
-        Console.WriteLine($"ID: {Id}, Description: {Description}");
+        public int Id { get; set; } = id;
+        public string Description { get; set; } = description;
+
+        // public Entity(int id, string description)
+        // {
+        //     Id = id;
+        //     Description = description;
+        // }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {Id}, Description: {Description}");
+        }
     }
 }
